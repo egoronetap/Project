67fc3b2b48d02b84cc9ea033aa6bb1f2bf0c5c80
egoronetap <59743249+egoronetap@users.noreply.github.com>
2022-02-25 17:12:38 +0300

    Игра по системам счисления

---
+++ constants.py
@@ -0,0 +1,11 @@
+# Egor
+SCREEN_WIDTH = 710
+SCREEN_HEIGHT = 710
+
+SCREEN_TITLE = 'Супер игра 228'
+
+STAGE = 'Меню'
+
+DB = 'db/scores.db'
+
+STATUS = False
---
+++ main_2.py
@@ -0,0 +1,55 @@
+# Egor + Anfisa (только что касается кнопки "Играть")
+import constants
+import pygame
+from menu_2 import IntroductionView, Settings, Authorization, Results, User
+from my_functions import terminate
+from main_game import play
+from store import store
+from personalization import personalization
+
+
+def main():
+    user = User()
+    view = IntroductionView(user)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.MOUSEMOTION:
+                if constants.STAGE == 'Меню':
+                    view.animate()
+                elif constants.STAGE == 'Магазин':
+                    sett = Settings(user)
+                    sett.animate()
+                elif constants.STAGE == 'Войти в аккаунт':
+                    auth = Authorization(user)
+                    auth.animate()
+                elif constants.STAGE == 'Результаты':
+                    res = Results(user)
+                    res.animate()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if constants.STAGE == 'Меню':
+                    view.push_btn()
+                elif constants.STAGE == 'Магазин':
+                    store(user.name)
+                    constants.STAGE = 'Меню'
+                    view = IntroductionView(user)
+                elif constants.STAGE == 'Результаты':
+                    res = Results(user)
+                    res.push_btn()
+                elif constants.STAGE == 'Играть':
+                    font, backgr, mode, difficulty = personalization(user.name)
+                    if 'из' in mode:
+                        from_sys = 2
+                        to_sys = 16 if 'шест' in difficulty else 8 if 'вос' in difficulty else 4
+                    else:
+                        to_sys = 2
+                        from_sys = 16 if 'шест' in difficulty else 8 if 'вос' in difficulty else 4
+                    play(user.name, font_name=font, fon_img=f'{backgr}.jpg', to_sys=to_sys, from_sys=from_sys)
+                    constants.STAGE = 'Меню'
+                    view = IntroductionView(user)
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    main()
